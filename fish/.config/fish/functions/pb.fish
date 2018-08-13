0ce6ea3b235b10c8f799c1cd350589499e2fbd65
Ben Harris <ben@tilde.team>
2018-08-13 16:34:39 -0400

    update pb function, add blinkenshell ssl fp

--- fish/.config/fish/functions/pb.fish
+++ fish/.config/fish/functions/pb.fish
@@ -1,4 +1,8 @@
 function pb
-    nc tilde.team 9999
+    if count $argv > /dev/null
+        curl -F'file=@'$argv[1] https://0x0.tilde.team
+    else
+        curl -F'file=@-;filename=stdin.txt' https://0x0.tilde.team
+    end
 end
 
