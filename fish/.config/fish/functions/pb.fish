a59c4b3955c0d7ff1cf9fb268c169ae9c547516e
Ben Harris <ben@tilde.team>
2018-08-13 20:18:49 -0400

    add check for existing pb command

--- fish/.config/fish/functions/pb.fish
+++ fish/.config/fish/functions/pb.fish
@@ -1,8 +1,12 @@
 function pb
-    if count $argv > /dev/null
-        curl -F'file=@'$argv[1] https://0x0.tilde.team
+    if type -q pb > /dev/null
+        command pb $argv
     else
-        curl -F'file=@-;filename=stdin.txt' https://0x0.tilde.team
+        if count $argv > /dev/null
+            curl -F'file=@'$argv[1] https://0x0.tilde.team
+        else
+            curl -F'file=@-;filename=stdin.txt' https://0x0.tilde.team
+        end
     end
 end
 
