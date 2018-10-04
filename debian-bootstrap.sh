a21f2a991b063a623940f800bbcc5095ec49dc9c
Ben Harris <ben@tilde.team>
2018-10-03 21:28:19 -0400

    add extra sources

--- debian-bootstrap.sh
+++ debian-bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "deb http://repo.vivaldi.com/stable/deb/ stable main" > /etc/apt/sources.list.d/vivaldi.list
+echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+
 apt update
 apt upgrade -y
 apt full-upgrade -y
