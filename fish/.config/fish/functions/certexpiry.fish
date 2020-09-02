5fb97e1d3a10b97cbbecb5c95d6da6099e30e1f4
Ben Harris <ben@tilde.team>
2020-09-02 09:22:26 -0400

    pipe dev null to certexpiry fn

--- fish/.config/fish/functions/certexpiry.fish
+++ fish/.config/fish/functions/certexpiry.fish
@@ -1,4 +1,3 @@
 function certexpiry
-    openssl s_client -connect $argv[1] 2>/dev/null | openssl x509 -noout -dates
+    openssl s_client -connect $argv[1] </dev/null 2>/dev/null | openssl x509 -noout -dates
 end
-
