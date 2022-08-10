ab9da2a8b4eafd49bb2f98812d9f64653370a0aa
Ben Harris <ben@tilde.team>
2022-08-10 15:05:22 -0400

    tidy up config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,21 +5,38 @@ if test -d ~/Maildir
   set -x MAIL ~/Maildir
 end
 
-for i in ~/bin ~/.yarn/bin ~/.rbenv/bin ~/.local/bin ~/.cargo/bin
-  if test -d $i && not contains $i $PATH
-    set -x fish_user_paths $fish_user_paths $i
+for i in ~/bin ~/.local/bin ~/.rbenv/bin ~/.rbenv/shims ~/.cargo/bin ~/.yarn/bin
+  if test -d $i
+    fish_add_path --path $i
   end
 end
 
 # add rbenv paths
-status --is-interactive && type -q rbenv && source (rbenv init -|psub)
+if status --is-interactive && type -q rbenv
+  set -gx RBENV_SHELL fish
+  source '/usr/lib/rbenv/completions/rbenv.fish'
+
+  command rbenv rehash 2>/dev/null
+
+  function rbenv
+    set command $argv[1]
+    set -e argv[1]
+
+    switch "$command"
+    case rehash shell
+      source (rbenv "sh-$command" $argv|psub)
+    case '*'
+      command rbenv "$command" $argv
+    end
+  end
+end
 
 #set -x SSH_AGENT_PID ""
 if test -S $HOME/.gnupg/S.gpg-agent
   set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
   set -x GPG_SOCK $HOME/.gnupg/S.gpg-agent
 else
-  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  type -q gpgconf && set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 end
 
 set -x BBJ_USER $USER
