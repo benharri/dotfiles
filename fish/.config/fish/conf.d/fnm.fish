260369bba4db79af6f16645ce10bbd2c6cf22b40
Ben Harris <ben@tilde.team>
2025-01-07 18:54:32 -0500

    tidy fish

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -1,5 +1,3 @@
-# fnm
-fish_add_path --path ~/.local/share/fnm
 if command -q fnm
     fnm env | source
 end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,41 +1,13 @@
 if test -d ~/Maildir
-  set -x MAIL ~/Maildir
+    set -x MAIL ~/Maildir
 end
 
-for i in ~/bin ~/.local/bin ~/.rbenv/bin ~/.rbenv/shims ~/.cargo/bin ~/.yarn/bin ~/go/bin ~/.dotnet/tools
-  fish_add_path --path $i
+for i in ~/bin ~/.local/bin ~/.rbenv/bin ~/.rbenv/shims ~/.cargo/bin ~/.yarn/bin ~/go/bin ~/.dotnet/tools ~/.local/share/fnm
+    fish_add_path --path $i
 end
 
-# add rbenv paths
-if status --is-interactive && type -q rbenv
-  set -gx RBENV_SHELL fish
-  source '/usr/lib/rbenv/completions/rbenv.fish'
-
-  command rbenv rehash 2>/dev/null
-
-  function rbenv
-    set command $argv[1]
-    set -e argv[1]
-
-    switch "$command"
-    case rehash shell
-      source (rbenv "sh-$command" $argv|psub)
-    case '*'
-      command rbenv "$command" $argv
-    end
-  end
-end
-
-if test -S $HOME/.gnupg/S.gpg-agent
-  set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent
-  set -x GPG_SOCK $HOME/.gnupg/S.gpg-agent
-else
-  type -q gpgconf && set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-end
-
-# add dotnet completions if needed
 if status --is-interactive && type -q dotnet
-  complete -f -c dotnet -a "(dotnet complete)"
+    complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
 end
 
 status --is-login && status --is-interactive && exec byobu-launcher
